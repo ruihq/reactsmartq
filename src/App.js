9fc1125125f779d2e8a18ff4f95c9d50b8bf93b8
Lunime ૮ ˶ᵔ ᵕ ᵔ˶ ა <120704613+ruihq@users.noreply.github.com>
2023-01-12 05:36:33 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div class="topnav">
+        <a class="active" href="index.html">
+          Home
+        </a>
+        <a href="https://github.com/ruihq/smartq">GitHub</a>
+        <a href="https://ruihq.github.io/SmartQ/versions.html">Download</a>
+        <a href="https://ruihq.github.io/SmartQ/about.html">About</a>
+      </div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
